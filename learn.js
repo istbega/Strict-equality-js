47143d579e4f897331822297ec12b5fa938d4528
bega <behinmaksym@gmail.com>
2024-11-12 14:10:52 +0000

    Add files via upload

---
+++ learn.js
@@ -0,0 +1,37 @@
+//  = assignment operator
+// == comparison operator ( compare if values are equal )
+// === strict equalityy operator ( compare if values & datatype are equal )
+// != inequality opearotr
+// !== strict inequality opeartor
+
+//const PI = 3.14;
+
+//if (PI == "3.14") {
+// That is PI ==
+//console.log("That is PI");
+//} else {
+//console.log("That is NOT Pi");
+//}
+
+//if (PI === "3.14") {
+// 3.14 That is NOT PI
+// "3.14 That is PI"
+// That is NOT PI ===
+//console.log("That is PI");
+//} else {
+//console.log("That is NOT Pi");
+//}
+
+//if (PI != "3.14") {
+//console.log("That NOT PI"); // console = That is PI
+//} else {
+//console.log("That is PI");
+//}
+
+//const PI = "3.14";
+
+//if (PI !== "3.14") {
+//onsole.log("That NOT PI"); // console = That is PI
+//} else {
+//console.log("That is PI");
+//}
